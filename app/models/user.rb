c91c1eb337996dd3aaba1f2111af79f6083a4be3
mk-67 <s100.maeda.kosuke@gmail.com>
2021-05-27 17:04:56 +0900

    バリデーション設定2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   with_options presence: true do
-    validates :encrypted_password,    format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
-    validates :password,              format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
-    validates :password_confirmation, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
+    validates :password,        format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
     validates :nickname
-    validates :last_name,             format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-    validates :first_name,            format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-    validates :last_name_kana,        format:{with: /\A[ァ-ヶ]+\z/}
-    validates :first_name_kana,       format:{with: /\A[ァ-ヶ]+\z/}
+    validates :last_name,       format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :first_name,      format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :last_name_kana,  format: {with: /\A[ァ-ヶ]+\z/}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶ]+\z/}
     validates :birthday
   end
 end
