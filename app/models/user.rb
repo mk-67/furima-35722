ecde5daf71bdf5bc56488550a3de214fd8259277
mk-67 <s100.maeda.kosuke@gmail.com>
2021-05-27 15:35:01 +0900

    新規登録〜ログイン有無実装
    ・deviseパラメーター設定
    ・バリデーション修正
    ・フォームと:hogeを修正
    ・ログイン有無条件分岐

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana,  presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday,        presence: true
+  
+  with_options presence: true do
+    validates :encrypted_password,    format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
+    validates :password,              format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
+    validates :password_confirmation, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
+    validates :nickname
+    validates :last_name,             format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :first_name,            format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :last_name_kana,        format:{with: /\A[ァ-ヶ]+\z/}
+    validates :first_name_kana,       format:{with: /\A[ァ-ヶ]+\z/}
+    validates :birthday
+  end
 end
