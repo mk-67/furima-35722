0b67fe6eaf3eca251e256e6d269b7c2c30b9d6b4
mk-67 <s100.maeda.kosuke@gmail.com>
2021-05-31 14:20:37 +0900

    active_hash導入・投稿機能実装〜FactoryBot実装
    ※クラス名はスネークキャメルではなくアッパーキャメルケースで記述する。

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,27 @@
 class Product < ApplicationRecord
-  belongs_to :user
+  belongs_to       :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to       :category
+  belongs_to       :status
+  belongs_to       :shipping_burden
+  belongs_to       :prefecture
+  belongs_to       :shipping_day
 
   with_options presence: true do
     validates :name
     validates :description
+    validates :price
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1 } do
     validates :category_id
     validates :status_id
     validates :shipping_burden_id
     validates :prefecture_id
     validates :shipping_day_id
-    validates :price
-    validates :image
   end
+
 end
