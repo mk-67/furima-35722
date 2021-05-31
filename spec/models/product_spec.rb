da79bdea35bf4f0d8acd2fc3273bfa14c09e45a2
mk-67 <s100.maeda.kosuke@gmail.com>
2021-05-31 19:02:01 +0900

    テストコード〜JavaScript設定

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,172 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @product = FactoryBot.build(:product)
+  end
+
+  describe '商品出品機能' do
+    context '商品出品できるとき' do
+      it '全項目が存在すれば出品できる' do
+        expect(@product).to be_valid
+      end
+      it 'nameが40文字だと出品できる' do
+        @product.name = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        expect(@product).to be_valid
+      end
+      it 'descriptionが1000文字だと出品できる' do
+        @product.description = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        expect(@product).to be_valid
+      end
+      it 'priceが半角数字だと出品できる' do
+        @product.price = '10000'
+        expect(@product).to be_valid
+      end
+      it 'priceが¥300だと出品できる' do
+        @product.price = '300'
+        expect(@product).to be_valid
+      end
+      it 'priceが¥9,999,999だと出品できる' do
+        @product.price = '9999999'
+        expect(@product).to be_valid
+      end
+      it 'category_idが2だと出品できる' do
+        @product.category_id = 2
+        expect(@product).to be_valid
+      end
+      it 'status_idが2だと出品できる' do
+        @product.status_id = 2
+        expect(@product).to be_valid
+      end
+      it 'shipping_burden_idが2だと出品できる' do
+        @product.shipping_burden_id = 2
+        expect(@product).to be_valid
+      end
+      it 'prefecture_idが2だと出品できる' do
+        @product.prefecture_id = 2
+        expect(@product).to be_valid
+      end
+      it 'shipping_day_idが2だと出品できる' do
+        @product.shipping_day_id = 2
+        expect(@product).to be_valid
+      end
+    end
+    context '商品出品できないとき' do
+      it 'imageが空では出品できない' do
+        @product.image = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空では出品できない' do
+        @product.name = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'nameが41文字だと出品できない' do
+        @product.name = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
+      end
+      it 'descriptionが空では出品できない' do
+        @product.description = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'descriptionが1001文字だと出品できない' do
+        @product.description = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Description is too long (maximum is 1000 characters)")
+      end
+      it 'category_idが空では出品できない' do
+        @product.category_id = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'category_idが1では出品できない' do
+        @product.category_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'status_idが空では出品できない' do
+        @product.status_id = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Status can't be blank")
+      end
+      it 'status_idが1では出品できない' do
+        @product.status_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Status must be other than 1")
+      end
+      it 'shipping_burden_idが空では出品できない' do
+        @product.shipping_burden_id = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Shipping burden can't be blank")
+      end
+      it 'shipping_burden_idが1では出品できない' do
+        @product.shipping_burden_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Shipping burden must be other than 1")
+      end
+      it 'prefecture_idが空では出品できない' do
+        @product.prefecture_id = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'prefecture_idが1では出品できない' do
+        @product.prefecture_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it 'shipping_day_idが空では出品できない' do
+        @product.shipping_day_id = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it 'shipping_day_idが1では出品できない' do
+        @product.shipping_day_id = 1
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Shipping day must be other than 1")
+      end
+      it 'priceが空では出品できない' do
+        @product.price = ''
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが全角数字では出品できない' do
+        @product.price = '１１１'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is invalid")
+      end
+      it 'priceが全角では出品できない' do
+        @product.price = 'あああ'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is invalid")
+      end
+      it 'priceが半角英字では出品できない' do
+        @product.price = 'aaa'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is invalid")
+      end
+      it 'priceが半角英数字混合では出品できない' do
+        @product.price = '111aa'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが¥299では出品できない' do
+        @product.price = '299'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが¥10,000,000では出品できない' do
+        @product.price = '10000000'
+        @product.valid?
+        expect(@product.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it '紐づくユーザーが存在しないと出品できない' do
+        @product.user = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
