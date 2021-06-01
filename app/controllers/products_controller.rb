28f5cba67af64af2c161f50957d7577a050b5a96
mk-67 <s100.maeda.kosuke@gmail.com>
2021-06-01 17:07:29 +0900

    authenticate_user!変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :maeda_params, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
 
