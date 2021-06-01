a84e12cfc942d0b278950d25e141f037213c886f
mk-67 <s100.maeda.kosuke@gmail.com>
2021-06-01 16:40:07 +0900

    商品情報編集機能実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,22 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    unless user_signed_in? && current_user.id == @product.user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :category_id, :status_id, :shipping_burden_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
