a6296a1346c1b125930411af40ab549af2dff0f6
mk-67 <s100.maeda.kosuke@gmail.com>
2021-06-01 17:00:05 +0900

    before_actionで可読性アップ

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :maeda_params, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
+
   def index
     @product = Product.includes(:user).order("created_at DESC")
   end
@@ -17,18 +20,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def edit
-    @product = Product.find(params[:id])
-    unless user_signed_in? && current_user.id == @product.user.id
-      redirect_to action: :index
-    end
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to product_path(@product.id)
     else
@@ -39,5 +36,15 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:name, :description, :category_id, :status_id, :shipping_burden_id, :prefecture_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def maeda_params
+    @product = Product.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @product.user.id
+      redirect_to action: :index
+    end
   end
 end
