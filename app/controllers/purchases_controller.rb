28ffb0fa955a0100044c9987a8bdc3751d0ab50a
mk-67 <s100.maeda.kosuke@gmail.com>
2021-06-03 17:49:46 +0900

    クレジットカード決済機能実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,15 @@
 class PurchasesController < ApplicationController
 
   def index
+    @product = Product.find(params[:product_id])
     @purchase_address = PurchaseAddress.new
   end
 
   def create
+    @product = Product.find(params[:product_id])
     @purchase_address = PurchaseAddress.new(purchase_address_params)
     if @purchase_address.valid?
+      pay_product
       @purchase_address.save
       redirect_to root_path
     else
@@ -16,6 +19,15 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_address_params
-    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id], purchase_id: params[:purchase_id])
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, product_id: params[:product_id], token: params[:token])#, purchase_id: params[:purchase_id])
+  end
+
+  def pay_product
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: Product.find(purchase_address_params[:product_id]).price, 
+        card: purchase_address_params[:token], 
+        currency: 'jpy'
+      )
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,11 +1,13 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :product_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :purchase_id
+  attr_accessor :user_id, :product_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :token#, :purchase_id
 
   with_options presence: true do
     validates :user_id
     validates :product_id
   end
+
+  validates :token, presence: true
 
   with_options presence: true do
     validates :postal_code,  format: {with: /\A\d{3}[-]\d{4}\z/}
@@ -13,12 +15,11 @@ class PurchaseAddress
     validates :municipality
     validates :address
     validates :phone_number, format: {with: /\A\d{11}\z/}
-    validates :purchase_id
   end
 
   def save
     purchase = Purchase.create(user_id: user_id, product_id: product_id)
-    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase_id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 
 end
