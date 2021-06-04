6f2148ba52c7e7741f8bce7e34e3855d377ecb9b
mk-67 <s100.maeda.kosuke@gmail.com>
2021-06-04 14:12:53 +0900

    バリデーション修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -11,7 +11,7 @@ class PurchaseAddress
 
   with_options presence: true do
     validates :postal_code,  format: {with: /\A\d{3}[-]\d{4}\z/}
-    validates :prefecture_id
+    validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipality
     validates :address
     validates :phone_number, format: {with: /\A\d{10,11}\z/}
